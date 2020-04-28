dd5b18e5715c998268431ced3ba6ae0142ba367a
jaguzman <jaguzman1@miuandes.cl>
2020-04-27 20:38:16 -0400

    ahora si listo

---
+++ Lab_5/Lab_5/SendEmailEventArgs.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_5
+{
+    class SendEmailEventArgs : EventArgs
+    {
+        public string Email { get; set; }
+        public string Username { get; set; }
+    }
+}
