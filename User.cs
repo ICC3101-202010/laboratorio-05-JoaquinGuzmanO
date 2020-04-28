3755623ae5c28b6b48f0582129976cd3fe11d583
jaguzman <jaguzman1@miuandes.cl>
2020-04-27 20:32:09 -0400

    listo

---
+++ User.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Lab_5
+{
+    class User
+    {
+        public delegate void SendEmailEventHandler(object source, EventArgs args);
+        public event SendEmailEventHandler EmailVerified;
+        protected virtual void OnEmailVerified()
+        {
+            EmailVerified(this, EventArgs.Empty);
+        }
+
+        public void OnEmailSent(object source, EventArgs args)
+        {
+            Console.WriteLine("desea verificar su cuenta? [si] [no]");
+            string answer = Console.ReadLine();
+            if(answer == "si")
+            {
+                OnEmailVerified();
+            }
+            else
+            {
+                Console.WriteLine("Su cuenta no ha sido verificada");
+            }
+        }
+        public void OnEmailVerified(object source, EventArgs args)
+        {
+            Thread.Sleep(2000);
+            Console.WriteLine($"\nSu correo ha sido verificado");
+            Thread.Sleep(2000);
+        }
+    }
+}
